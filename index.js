7ab090f8a18ec259fbb73f01e916d40f560f3b7e
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2024-08-02 09:36:24 +0200

    Code Refactoring.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 mongoose.connect('mongodb://localhost/vidly-backend')
     .then(() => console.log("Connected to mongoDb"))
-    .catch(err => console.error("Couldnot connect to mongoDb", err));
+    .catch(err => console.error("Could not connect to mongoDb", err));
 
 const PORT = process.env.PORT || 4000;
 
