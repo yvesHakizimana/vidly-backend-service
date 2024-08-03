4d30b3605260f36d5ae12b3393cbd38de7af530e
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2024-08-03 08:15:36 +0200

    Code Refactoring.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const genres = require('./routes/genres')
+const customers = require('./routes/customers')
 
 app.use(express.json());
 
@@ -11,10 +12,8 @@ mongoose.connect('mongodb://localhost/vidly-backend')
 
 const PORT = process.env.PORT || 4000;
 
-
-
 app.use('/api/genres', genres);
-
+app.use('/api/customers', customers);
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
