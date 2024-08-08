be08b33655b8cf61aad3905628d5b075df1d16f5
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2024-08-08 10:51:29 +0200

    adding bcrypt password hashing.

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const genres = require('./routes/genres')
 const customers = require('./routes/customers')
 const movies = require('./routes/movies')
 const rentals = require('./routes/rentals')
+const users = require('./routes/users')
 
 app.use(express.json());
 
@@ -20,6 +21,7 @@ app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals)
+app.use('/api/users', users)
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
