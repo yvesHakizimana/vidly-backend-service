211b96efe8b3d95cb538e81ba67434f14c2e9bb2
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2024-08-08 11:23:35 +0200

    Adding auth/signin module.

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const customers = require('./routes/customers')
 const movies = require('./routes/movies')
 const rentals = require('./routes/rentals')
 const users = require('./routes/users')
+const auth  = require('./routes/auth')
 
 app.use(express.json());
 
@@ -22,6 +23,7 @@ app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals)
 app.use('/api/users', users)
+app.use('/api/auth', auth)
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
---
+++ routes/auth.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const { User} = require('../models/user');
+const bcrypt = require("bcrypt");
+const Joi = require("joi");
+
+router.post('/', async (req, res) => {
+    //Validating the request in general
+    const {error} = validate(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    //Validating the username
+    let user = await User.findOne({ email: req.body.email })
+    if(!user) return res.status(400).send("Invalid email or password")
+
+    //Validating the password
+    const passwordValid = await bcrypt.compare(req.body.password, user.password)
+    if(!passwordValid) return res.status(400).send("Invalid email or password")
+
+    res.send(true)
+})
+
+function validate(user){
+    const userSchema = Joi.object({
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(5).max(255).required(),
+    })
+    return userSchema.validate(user)
+}
+
+module.exports = router
+
