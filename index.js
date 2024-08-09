14ad20d0abf8dc457df9f51df91583c3b87c0a10
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2024-08-09 17:24:01 +0200

    Refactoring project:: Building separate file for routes.

--- index.js
+++ index.js
@@ -5,15 +5,9 @@ const mongoose = require('mongoose')
 const config = require('config')
 const Joi = require('@hapi/joi');
 Joi.objectId = require('joi-objectid')(Joi);
-const genres = require('./routes/genres')
-const customers = require('./routes/customers')
-const movies = require('./routes/movies')
-const rentals = require('./routes/rentals')
-const users = require('./routes/users')
-const auth  = require('./routes/auth')
-const error = require('./middleware/error')
 const winston = require('./utils/logger');
 const {modifiers: ex} = require("@hapi/joi/lib/types/any");
+require('./startup/routes')(app)
 
 
 process.on('uncaughtException', ex => {
@@ -21,15 +15,10 @@ process.on('uncaughtException', ex => {
     process.exit(1)
 })
 
-
 process.on('unhandledRejection', err => {
     winston.error(err.message, err);
     process.exit(1)
 })
-
-app.use(express.json());
-
-throw new Error("Something failed in the startup")
 
 //Checking if the jwt private key is defined.
 if(!config.get('jwtPrivateKey')){
@@ -41,17 +30,7 @@ mongoose.connect('mongodb://localhost/vidly-backend')
     .then(() => console.log("Connected to mongoDb"))
     .catch(err => console.error("Could not connect to mongoDb", err));
 
-const PORT = process.env.PORT || 4001;
-
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals)
-app.use('/api/users', users)
-app.use('/api/auth', auth)
-
-//Error Handling.
-app.use(error)
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
