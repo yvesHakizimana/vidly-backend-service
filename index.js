df8acb2b2a9b8f9a874713d415e01e50d94e44e7
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2024-08-09 17:40:51 +0200

    Refactoring project:: Extracting validation logic.

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-require('express-async-errors')
 const express = require('express')
 const app = express()
-const config = require('config')
-const Joi = require('@hapi/joi');
-Joi.objectId = require('joi-objectid')(Joi);
 const winston = require('./utils/logger');
 const {modifiers: ex} = require("@hapi/joi/lib/types/any");
 
-
+require('express-async-errors')
 require('./startup/routes')(app)
 require('./startup/db')()
+require('./startup/config')()
+require('./startup/validation')()
 
 process.on('uncaughtException', ex => {
     winston.error(ex.message, ex);
@@ -21,13 +19,7 @@ process.on('unhandledRejection', err => {
     process.exit(1)
 })
 
-//Checking if the jwt private key is defined.
-if(!config.get('jwtPrivateKey')){
-    console.error("Jwt private key is not defined")
-    process.exit(1)
-}
-
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.listen(PORT, () => winston.info(`Listening on port ${PORT}`));
 
